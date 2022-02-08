3ab8736c5ea224b6c02a85a92eac7dc4d27acac1
jurasora1010 <ikumi.hamana@rakus-partners.co.jp>
2022-02-08 17:51:40 +0900

    コード修正

--- routes/s3.js
+++ routes/s3.js
@@ -33,16 +33,3 @@ async function generateUploadURL() {
   return uploadURL;
 }
 module.exports.generateUploadURL = generateUploadURL;
-//  generateUploadURL = async () => {
-//   const rawBytes = await randomBytes(16);
-//   const imageName = rawBytes.toString("hex");
-
-//   const params = {
-//     Bucket: bucketName,
-//     Key: imageName,
-//     Expires: 60,
-//   };
-
-//   const uploadURL = await s3.getSignedUrlPromise("putObject", params);
-//   return uploadURL;
-// };
