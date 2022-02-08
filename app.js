791b3365a4af8b6f3edf98ec52471bbaa5f0c225
hidakaitsuki <itsuki.hidaka@rakus-partners.co.jp>
2022-02-08 14:23:03 +0900

    test

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(cors());
 app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
